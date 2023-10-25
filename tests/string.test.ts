a4342a1b630683e8e8ccb0804a1f319d6f766e4a
Albert <albert990624@gmail.com>
2023-10-25 21:37:15 +0800

    fix: remove useless test

--- tests/string.test.ts
+++
@@ -1,6 +0,0 @@
-import { upperCaseForTest } from './../src/utils/string';
-
-test('upperCase function should upper case a string', () => {
-  expect(upperCaseForTest('hello')).toBe('HELLO');
-  expect(upperCaseForTest('world')).toBe('WORLD');
-});
