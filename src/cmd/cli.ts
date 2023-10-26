3104cf758022da62c5c5682bf75fc0316b048de8
Albert <albert990624@gmail.com>
2023-10-26 15:28:15 +0800

    feat(cli): add cli parser

--- src/cmd/cli.ts
+++ src/cmd/cli.ts
@@ -1,15 +1,42 @@
 // examples/basic-usage.js
 import { cac } from 'cac';
 
-const cli = cac('aicli').version('0.0.1').help();
+const cli = cac('ai-cli').version('0.0.1').help();
+
+cli.option('-c, --config', 'Edit Config File');
+
+cli.option('-i, --info', 'Show Info');
 
 cli
-  .command('rm <dir>', 'Remove a dir')
-  .option('-r, --recursive', 'Remove recursively')
-  .action((dir, options) => {
-    console.log('remove ' + dir + (options.recursive ? ' recursively' : ''));
+  .command('explain', 'Explain')
+  .alias('e')
+  .alias('')
+  .action(() => {
+    console.log('Explain');
   });
 
-cli.help();
+cli
+  .command('complex', 'Complex Explain')
+  .alias('c')
+  .action(() => {
+    console.log('Complex Explain');
+  });
 
-cli.parse();
+cli
+  .command('transform', 'Transform Command')
+  .alias('t')
+  .action(() => {
+    console.log('transform');
+  });
+
+// TODO: if no params, show help
+
+cli.usage('Your AI helper');
+
+const parsed = cli.parse();
+
+if (parsed.options.config) {
+  console.log('Edit Config File');
+} else if (parsed.options.info) {
+  console.log('Show Info');
+}
